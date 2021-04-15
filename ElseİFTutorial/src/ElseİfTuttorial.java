5c8c6509e5b835e6c2e43eb00992ad1aac673dc8
abdulvahap önen <82175373+abdulvahaponen@users.noreply.github.com>
2021-04-15 21:11:42 +0300

    Add files via upload

---
+++ ElseİFTutorial/src/ElseİfTuttorial.java
@@ -0,0 +1,46 @@
+
+public class ElseÝfTuttorial {
+     public void kosul() {
+    	 int x=0;
+    	 if (x>0) {
+			int z=x*5;
+			System.out.println("z deðeri"+z);
+		}
+     }
+     public void kousl2() {
+    	 int x=55;
+    	 if (x>90) {
+			System.out.println("x 90 dan büyüktür");
+		}else if (x>=80) {
+			System.out.println("x 80 ile 90 arasý");
+		}else if (x>=70) {
+			System.out.println("x 80 ile 70 arasýndadýr");
+		}else if (x>=60) {
+			System.out.println("x 70 ile 60 arasýndadýr");
+		}else if (x>=50) {
+			System.out.println("x 60 ile 50 arasýndadýr");
+		}else if (x>=40) {
+			System.out.println("x 50 ile 40 arasýndadýr");
+		}else if (x>=30) {
+			System.out.println("x 40 ile 30 arasýndadýr");
+		}else if (x>=20) {
+			System.out.println("x 30 ile 20 arasýndadýr");
+		}else if (x>=10) {
+			System.out.println("x 20 ile 10 aradýndadýr");
+		}else if (x>=0) {
+			System.out.println("x 10 ile 0 arasýndadýr");
+		}else {
+			System.out.println("x 0 dan küçüktür");
+		}
+     }
+	public static void main(String[] args) {
+		
+	
+int x = 3;
+if(x>0) {
+	int z=x*5;
+	System.out.println("z deðeri:"+z);
+}
+
+}
+}
